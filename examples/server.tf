b8e6c85028fe5fbe0c9b7f58f320b93eb4783755
Nathan Bolam <nbolam@capturealpha.com>
2022-09-09 06:55:15 -0500

    CHANGE: Bump latitude-api-client to v0.0.13 FIX: SSH keys type conversion FIX: Typos is docs ADD: Installation instructions

--- examples/server.tf
+++ examples/server.tf
@@ -1,7 +1,7 @@
 resource "latitude_server" "server" {
   hostname = "foo"
   operating_system = "ubuntu_20_04_x64_lts"
-  plan = data.latitude_plan.plan.name
+  plan = data.latitude_plan.plan.slug
   project_id = latitude_project.project.id
   site = data.latitude_region.region.slug
   ssh_keys = [latitude_ssh_key.ssh_key.id]
