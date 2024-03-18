b8189d00184c9f6d7f125e42ad1bfdb20028f67d
Yuri Hummel <yurihummel@outlook.com>
2024-03-18 17:52:55 -0300

    feat: add tag resource

--- latitudesh/resource_tag.go
+++ latitudesh/resource_tag.go
@@ -1,9 +1,149 @@
 package latitudesh
 
 import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	api "github.com/latitudesh/latitudesh-go"
 )
+
+func resourceTag() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceTagCreate,
+		ReadContext:   resourceTagRead,
+		UpdateContext: resourceTagUpdate,
+		DeleteContext: resourceTagDelete,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:        schema.TypeString,
+				Description: "The tag name",
+				Required:    true,
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Description: "The tag description",
+				Required:    true,
+			},
+			"color": {
+				Type:        schema.TypeString,
+				Description: "The tag color",
+				Required:    true,
+			},
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: NestedResourceRestAPIImport,
+		},
+	}
+}
+
+func resourceTagCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	c := m.(*api.Client)
+
+	createRequest := &api.TagCreateRequest{
+		Data: api.TagCreateData{
+			Type: "Tag",
+			Attributes: api.TagCreateAttributes{
+				Name:        d.Get("name").(string),
+				Description: d.Get("description").(string),
+				Color:       d.Get("color").(string),
+			},
+		},
+	}
+
+	Tag, _, err := c.Tags.Create(createRequest)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(Tag.ID)
+
+	if d.Get("tags") != nil {
+		resourceTagUpdate(ctx, d, m)
+	}
+
+	resourceTagRead(ctx, d, m)
+
+	return diags
+}
+
+func resourceTagRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*api.Client)
+
+	var diags diag.Diagnostics
+
+	TagID := d.Id()
+
+	Tag, resp, err := GetTag(c, TagID)
+
+	if err != nil {
+		if resp.StatusCode == http.StatusNotFound {
+			d.SetId("")
+			return diags
+		}
+
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("name", &Tag.Name); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("description", &Tag.Description); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("color", &Tag.Color); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return diags
+}
+
+func resourceTagUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*api.Client)
+
+	TagID := d.Id()
+
+	updateRequest := &api.TagUpdateRequest{
+		Data: api.TagUpdateData{
+			Type: "Tags",
+			ID:   TagID,
+			Attributes: api.TagUpdateAttributes{
+				Name:        d.Get("name").(string),
+				Description: d.Get("description").(string),
+				Color:       d.Get("color").(string),
+			},
+		},
+	}
+
+	_, _, err := c.Tags.Update(TagID, updateRequest)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceTagRead(ctx, d, m)
+}
+
+func resourceTagDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	c := m.(*api.Client)
+
+	var diags diag.Diagnostics
+
+	TagID := d.Id()
+
+	_, err := c.Tags.Delete(TagID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+
+	return diags
+}
 
 func tagIDs(tags []api.EmbedTag) []string {
 	ids := []string{}
@@ -26,4 +166,24 @@ func parseTags(d *schema.ResourceData) []string {
 	}
 
 	return tags_slice
+}
+
+func GetTag(c *api.Client, tagID string) (*api.Tag, *api.Response, error) {
+	tags, resp, err := c.Tags.List(nil)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	for _, tag := range tags {
+		if tag.ID == tagID {
+			return &tag, resp, nil
+		}
+	}
+
+	resp.Status = "404 Not Found"
+	resp.StatusCode = http.StatusNotFound
+
+	notFoundErr := errors.New("ERROR\nStatus: 404\nSpecified Record Not Found")
+
+	return nil, resp, notFoundErr
 }
---
+++ latitudesh/resource_tag_test.go
@@ -0,0 +1,98 @@
+package latitudesh
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	api "github.com/latitudesh/latitudesh-go"
+)
+
+const (
+	testTagName        = "test_tag"
+	testTagDescription = "terraform test tag"
+	testTagColor       = "#ffffff"
+)
+
+func TestAccTag_Basic(t *testing.T) {
+	var Tag api.Tag
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccTokenCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckTagDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckTagBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTagExists("latitudesh_tag.test_item", &Tag),
+					resource.TestCheckResourceAttr(
+						"latitudesh_tag.test_item", "name", testTagName),
+					resource.TestCheckResourceAttr(
+						"latitudesh_tag.test_item", "description", testTagDescription),
+					resource.TestCheckResourceAttr(
+						"latitudesh_tag.test_item", "color", testTagColor),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckTagDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*api.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "latitudesh_tag" {
+			continue
+		}
+		if _, _, err := GetTag(client, rs.Primary.ID); err == nil {
+			return fmt.Errorf("Virtual network still exists")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckTagExists(n string, tag *api.Tag) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No Record ID is set")
+		}
+
+		client := testAccProvider.Meta().(*api.Client)
+
+		foundTag, _, err := GetTag(client, rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		if foundTag.ID != rs.Primary.ID {
+			return fmt.Errorf("Record not found: %v - %v", rs.Primary.ID, foundTag)
+		}
+
+		*tag = *foundTag
+
+		return nil
+	}
+}
+
+func testAccCheckTagBasic() string {
+	return fmt.Sprintf(`
+resource "latitudesh_tag" "test_item" {
+	name  	= "%s"
+  	description = "%s"
+  	color        = "%s"
+}
+`,
+		testTagName,
+		testTagDescription,
+		testTagColor,
+	)
+}
