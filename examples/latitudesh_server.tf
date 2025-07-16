ddadb28fa6b4fc6c7e1913f598e55b119a0d08e8
Leandro Parazito <leandroh@gmail.com>
2025-07-16 18:33:41 -0300

    docs(example): add interfaces output to latitudesh_server.tf example

--- examples/latitudesh_server.tf
+++ examples/latitudesh_server.tf
@@ -1,8 +1,12 @@
 resource "latitudesh_server" "server" {
-  hostname         = "terraform.latitude.sh"
+  hostname         = "terraform-latitudesh"
   operating_system = "ubuntu_22_04_x64_lts"
   plan             = data.latitudesh_plan.plan.slug
   project          = latitudesh_project.project.id      # You can use the project id or slug
   site             = data.latitudesh_region.region.slug # You can use the site id or slug
   ssh_keys         = [latitudesh_ssh_key.ssh_key.id]
+}
+
+output "server_state" {
+  value = latitudesh_server.terraform_server
 }
