f82351d4012c3fe86b403a4a2075568c04f0117e
Leandro Parazito <11623+leandroh@users.noreply.github.com>
2026-03-18 19:15:41 -0300

    test(virtual_network): fix compilation issues and add unknown project test

--- latitudesh/resource_virtual_network_test.go
+++ latitudesh/resource_virtual_network_test.go
@@ -8,10 +8,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
 	latitudeshgosdk "github.com/latitudesh/latitudesh-go-sdk"
 )
@@ -21,37 +19,15 @@ const (
 	testVNSite = "SAO2"
 )
 
-// Provider factories (ProtoV6 + framework)
-var testAccProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-	"latitudesh": providerserver.NewProtocol6WithError(New("test")()),
-}
-
-func testAccPreCheck(t *testing.T) {
-	if os.Getenv("TF_ACC") == "" {
-		t.Fatalf("TF_ACC must be set for acceptance tests")
-	}
-	if os.Getenv("LATITUDESH_AUTH_TOKEN") == "" {
-		t.Fatalf("LATITUDESH_AUTH_TOKEN must be set for acceptance tests")
-	}
-	if os.Getenv("LATITUDESH_TEST_PROJECT") == "" {
-		t.Fatalf("LATITUDESH_TEST_PROJECT must be set (project id/slug used in provider block)")
-	}
-}
-
-func newSDKClientFromEnv() (*latitudeshgosdk.Latitudesh, error) {
-	token := os.Getenv("LATITUDESH_AUTH_TOKEN")
-	if token == "" {
-		return nil, fmt.Errorf("LATITUDESH_AUTH_TOKEN not set")
-	}
-	return latitudeshgosdk.New(latitudeshgosdk.WithSecurity(token)), nil
-}
-
 func TestAccVirtualNetwork_Basic(t *testing.T) {
 	resourceName := "latitudesh_virtual_network.test_item"
 
 	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProviderFactories,
+		PreCheck: func() {
+			testAccTokenCheck(t)
+			testAccProjectCheck(t)
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories(),
 		CheckDestroy:             testAccCheckVirtualNetworkDestroy,
 		Steps: []resource.TestStep{
 			{
@@ -91,47 +67,35 @@ func testAccCheckVirtualNetworkDestroy(s *terraform.State) error {
 
 		resp, err := client.PrivateNetworks.Get(ctx, id)
 		if err == nil && resp != nil && resp.Object != nil && resp.Object.Data != nil &&
-			resp.Object.Data.ID != nil && *resp.Object.Data.ID == id {
+			resp.Object.Data.Data != nil && resp.Object.Data.Data.ID != nil && *resp.Object.Data.Data.ID == id {
 			return fmt.Errorf("virtual network still exists: %s", id)
 		}
 	}
 	return nil
 }
 
-func testAccCheckVirtualNetworkExists(n string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Not found: %s", n)
-		}
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("No Record ID is set")
-		}
-
-		client := testAccProvider.Meta().(*latitudeshgosdk.Latitudesh)
-		ctx := context.Background()
-
-		response, err := client.PrivateNetworks.Get(ctx, rs.Primary.ID)
-		if err != nil {
-			return err
-		}
-
-		if response.Object == nil || response.Object.Data == nil {
-			return fmt.Errorf("virtual network not found")
-		}
-
-		vnet := response.Object.Data
-		vnData := vnet.GetData()
-
-		if vnData == nil || vnData.GetID() == nil || *vnData.GetID() != rs.Primary.ID {
-			return fmt.Errorf("Record not found: %v", rs.Primary.ID)
-		}
-
-		return nil
+func newSDKClientFromEnv() (*latitudeshgosdk.Latitudesh, error) {
+	token := os.Getenv("LATITUDESH_AUTH_TOKEN")
+	if token == "" {
+		return nil, fmt.Errorf("LATITUDESH_AUTH_TOKEN not set")
 	}
+	return latitudeshgosdk.New(latitudeshgosdk.WithSecurity(token)), nil
 }
 
-func testAccCheckVirtualNetworkBasic() string {
+func testAccConfigVirtualNetworkWithProviderProject(project, desc, site string) string {
+	return fmt.Sprintf(`
+provider "latitudesh" {
+  project = "%s"
+}
+
+resource "latitudesh_virtual_network" "test_item" {
+  description = "%s"
+  site        = "%s"
+}
+`, project, desc, site)
+}
+
+func testAccCheckVirtualNetworkBasic(project, desc, site string) string {
 	return fmt.Sprintf(`
 terraform {
   required_providers {
@@ -150,4 +114,31 @@ resource "latitudesh_virtual_network" "test_item" {
   site        = "%s"
 }
 `, project, desc, site)
+}
+
+func TestAccVirtualNetwork_UnknownProject(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccTokenCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: true,
+				Config: `
+provider "latitudesh" {}
+
+resource "latitudesh_project" "test" {
+  name        = "tf-acc-unknown-project-vn"
+  environment = "Development"
+}
+
+resource "latitudesh_virtual_network" "test_item" {
+  description = "tf-acc-unknown-project"
+  site        = "SAO2"
+  project     = latitudesh_project.test.id
+}
+`,
+			},
+		},
+	})
 }
